4bc21dffb0c3992709fe355ba0a3bf714cd397cf
baohao <liaobaohao@gmail.com>
2023-05-30 18:09:08 +0200

    add run code

--- run.py
+++ run.py
@@ -240,7 +240,7 @@ def train():
     # Evaluation
     if args.do_eval:
         logger.info("*** Evaluate ***")
-        metrics = trainer.evaluate(metric_key_prefix="eval")
+        metrics = trainer.evaluate(eval_dataset=data_module["eval_dataset"], metric_key_prefix="eval")
         trainer.log_metrics("eval", metrics)
         trainer.save_metrics("eval", metrics)
         all_metrics.update(metrics)
