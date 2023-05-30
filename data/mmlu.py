285dc9c7a8c51e102b3f3565248596563c74f3f2
baohao <liaobaohao@gmail.com>
2023-05-30 20:37:24 +0200

    add run code

--- data/mmlu.py
+++ data/mmlu.py
@@ -244,8 +244,8 @@ class MMLUEvalCallback(transformers.TrainerCallback):
             results[f'mmlu_accuracy_{subject}'] = subject_score
             subject_scores.append(subject_score)
         results[f'mmlu_accuracy'] = np.mean(subject_scores)
-        print(results)
-        self.trainer.log(results)
+        self.trainer.log_metrics(results)
+        self.trainer.save_metrics(results)
         self.trainer.data_collator.source_max_len = source_max_len
 
 
