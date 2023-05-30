88a031a4e7acfc4e2c4f1b7c56b45b03af1368a6
baohao <liaobaohao@gmail.com>
2023-05-30 21:11:03 +0200

    add run code

--- data/mmlu.py
+++ data/mmlu.py
@@ -219,7 +219,7 @@ class MMLUEvalCallback(transformers.TrainerCallback):
         for batch in tqdm(data_loader, total=len(data_loader)):
             (loss, logits, labels) = self.trainer.prediction_step(self.trainer.model, batch, prediction_loss_only=False)
             # There are two tokens, the output, and eos token.
-            print(logits.size())
+            print(len(logits), logits[0].size())
             for i, logit in enumerate(logits):
                 label_non_zero_id = (batch['labels'][i] != -100).nonzero()[0][0]
                 print(label_non_zero_id, self.abcd_idx, labels, logit.size())
