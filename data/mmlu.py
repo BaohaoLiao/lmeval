309b79fe08eb77048757b3bfba26e18272d7ac59
baohao <liaobaohao@gmail.com>
2023-05-31 16:16:18 +0200

    add run code

--- data/mmlu.py
+++ data/mmlu.py
@@ -171,6 +171,7 @@ def make_mmlu_dataset(
             for k, v in SUBCATEGORIES.items():
                 if v[0] == c:
                     subjects[k] = v.append(category)
+    print(subjects)
 
     for i, (k, v) in enumerate(subjects.items()):
         subcateg_dataset = load_dataset(DATASET_NAME, k, split=split)
